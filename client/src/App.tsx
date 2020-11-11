9906e7f124ac402a649ecdcda128dffe6a52c540
AlonBru <indyalon@gmail.com>
2020-11-11 21:40:11 +0200

    Client ESLint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,18 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const name:string = 'Artie'
-  const reverse = (name:string):string => {
-    return  name.split('').reverse().join('')
-  }
+function App():JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. <br/>
-          {reverse(name)}
+          Edit
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to reload.
+          {' '}
         </p>
         <a
           className="App-link"
