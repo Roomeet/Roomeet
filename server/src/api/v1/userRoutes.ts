1643b8ae20e4e27feffb4b3cb0c2292c5fa826ab
itaibn9 <itaibn9@gmail.com>
2020-12-09 22:59:18 +0200

    login and register passed and jest wxit operation at the end

--- server/src/api/v1/userRoutes.ts
+++ server/src/api/v1/userRoutes.ts
@@ -1,60 +1,139 @@
 import { Router, Request, Response } from 'express';
+
 import { ObjectId } from 'mongodb';
+
+
+
+// interfaces:
+
+
+
+// mongoDB models:
+
+import User from '../../../models/user';
+
+
 const router = Router();
 
-//interfaces:
-
-//mongoDB models:
-import User from '../../../models/user';
 const UserData = require('../../../models/userData');
+
+
 
 // Routes
 
+
+
 // Get all users
-router.get('/', /*authenticateToken ,*/ async (req: Request, res: Response) => {
+
+router.get('/', /* authenticateToken , */ async (req: Request, res: Response) => {
+
   try {
-    const users = await User.find({})
-    res.json(users);    
+
+    const users = await User.find({});
+
+    res.json(users);
+
   } catch (error) {
+
     res.status(500).json({ error });
+
   }
+
 });
+
+
 
 // Get single user for client context
-router.get('/email/:email', /*authenticateToken ,*/ async (req: Request, res: Response) => {
-  const email = req.params.email
-  
+
+router.get('/email/:email', /* authenticateToken , */ async (req: Request, res: Response) => {
+
+  const { email } = req.params;
+
+
+
   try {
-    const user = await User.find({email})
+
+    const user = await User.find({ email });
+
     res.json({
+
       id: user[0].id,
+
       name: user[0].name,
+
       lastName: user[0].lastName,
+
       email: user[0].email
-    });    
-  } catch (error) {
-    res.status(500).json({ error });
-  }
-});
 
-// Post user data
-router.post('/user-data', (req: Request, res: Response) => {
-  try {
-    const { body: rawUserData } = req;
-
-    const userData = new UserData({
-      ...rawUserData,
-      _id: new ObjectId(),
-      userId: new ObjectId(rawUserData.userId),
-      createdAt: new Date(),
-      updatedAt: null,
-      deletedAt: null,
     });
 
-    userData.save(userData).then(() => res.status(201).json('Updated info!'));
   } catch (error) {
+
     res.status(500).json({ error });
+
   }
+
 });
 
+
+
+// Post user data
+
+router.post('/user-data', (req: Request, res: Response) => {
+
+  try {
+
+    const { body: rawUserData } = req;
+
+
+
+    const userData = new UserData({
+
+      ...rawUserData,
+
+      _id: new ObjectId(),
+
+      userId: new ObjectId(rawUserData.userId),
+
+      createdAt: new Date(),
+
+      updatedAt: null,
+
+      deletedAt: null
+
+    });
+
+
+
+    userData.save(userData).then(() => res.status(201).json('Updated info!'));
+
+  } catch (error) {
+
+    res.status(500).json({ error });
+
+  }
+
+});
+
+
+
+router.delete('/all', /* authenticateToken , */ async (req: Request, res: Response) => {
+
+  try {
+
+    const user = await User.remove({});
+
+    res.status(200).json(user);
+
+  } catch (error) {
+
+    res.status(500).json({ error });
+
+  }
+
+});
+
+
+
 export default router;
+
--- server/src/tests/SigninOut.test.ts
+++ server/src/tests/SigninOut.test.ts
@@ -1,7 +1,10 @@
 /* eslint-disable no-undef */
+import app from '../app';
+
+const mongoose = require('mongoose');
+
 const request = require('supertest');
 // const app = require('../app');
-import app from '../app'
 
 // mock users
 const userSignupMock = {
@@ -11,20 +14,39 @@ const userSignupMock = {
   email: 'test@gmail.com'
 };
 
+const userSigninMock = {
+  email: 'test@gmail.com',
+  password: 'test1234',
+  rememberMe: false
+};
+
 describe('Register and Login Tests', () => {
-  jest.setTimeout(30000)
-  // afterAll(async () => {
-  //   await app.connec();
-  // });
+  jest.setTimeout(30000);
+  afterAll(async (done) => {
+    await request(app).delete('/api/v1/users/all');
+    await mongoose.connection.close();
+    done();
+  });
 
   // Register
   test('User Can Register', async (done) => {
-
     const signUpResponse = await request(app)
       .post('/api/v1/auth/register')
       .send(userSignupMock);
 
     expect(signUpResponse.status).toBe(201);
+    done();
+  });
+
+  // user login
+  test('User Can Login', async (done) => {
+    const loginResponse = await request(app)
+      .post('/api/v1/auth/login')
+      .send(userSigninMock);
+
+    expect(loginResponse.status).toBe(200);
+    expect(loginResponse.body.accessToken.length > 0).toBe(true);
+    expect(loginResponse.body.email).toBe(userSigninMock.email);
     done();
   });
 });
