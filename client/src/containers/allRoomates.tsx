ffc43819968eb27108b07e90784c914cbf985fda
Omri Zilberstein <67857576+zilbers@users.noreply.github.com>
2020-12-10 11:05:06 +0200

    Delete allRoomates.tsx

--- client/src/containers/allRoomates.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-
-const AllRoomates: React.FC = () => (
-  <>
-  </>
-);
-
-export default AllRoomates;
