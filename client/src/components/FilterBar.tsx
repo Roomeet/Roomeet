0d349bd3c799107e51b69bef23b9b078e4ca0e58
itaibn9 <itaibn9@gmail.com>
2020-12-29 16:29:37 +0200

    filter roomates works in a mobile as a pop up modal

--- client/src/components/FilterBar.tsx
+++ client/src/components/FilterBar.tsx
@@ -11,6 +11,7 @@ import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import network from '../utils/network';
+import FilterModal from './FilterModal';
 
 export type Props = {
     [key: string]: any;
@@ -25,22 +26,51 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
   range_root: {
     width: 150,
   },
-  searchBar: {
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  searchBarMobile: {
+    display: 'none',
+    [theme.breakpoints.down('sm')]: {
+      display: 'flex',
+    },
+  },
+  searchBarDesktop: {
     display: 'flex',
     flexDirection: 'row',
-    marginTop: '30px',
+    marginTop: '10px',
+    margin: 'auto',
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
   },
   buttons: {
     display: 'flex',
     flexDirection: 'column',
     marginLeft: '30px',
+    margin: 'auto',
+    [theme.breakpoints.down('sm')]: {
+
+    },
   },
   formControl: {
     margin: theme.spacing(1),
     minWidth: 120,
+    [theme.breakpoints.down('sm')]: {
+
+    },
   },
   selectEmpty: {
     marginTop: theme.spacing(2),
+  },
+  header: {
+    margin: 'auto',
+    [theme.breakpoints.down('sm')]: {
+
+    },
   },
 }));
 
@@ -56,7 +86,7 @@ const FilterBar: React.FC<Props> = ({ setAllUsersInfo, userId }) => {
     budgetRange: [500, 6000],
     ageRange: [16, 60],
   });
-
+  const [openFilter, SetOpenFilter] = useState<boolean>(false);
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFilters({ ...filters, [event.target.name]: event.target.checked });
   };
@@ -101,127 +131,140 @@ const FilterBar: React.FC<Props> = ({ setAllUsersInfo, userId }) => {
   };
 
   return (
-    <FormGroup className={classes.searchBar}>
-      <Typography variant="h4">
-        Filter Roomates
-      </Typography>
-      <FormControl variant="filled" className={classes.formControl}>
-        <InputLabel id="demo-simple-select-filled-label">Gender</InputLabel>
-        <Select
-          labelId="demo-simple-select-filled-label"
-          id="demo-simple-select-filled"
-          defaultValue={filters.gender}
-          name="gender"
-          value={filters.gender}
-          onChange={(event) => handleGenderChange(event)}
-        >
-          <MenuItem value="">
-            <em>None</em>
-          </MenuItem>
-          <MenuItem value="male">Male</MenuItem>
-          <MenuItem value="female">Female</MenuItem>
-        </Select>
-      </FormControl>
-      <div className={classes.range_root}>
-        <Typography id="range-slider" gutterBottom>
-          Age range
+    <>
+      <FormGroup className={classes.searchBarDesktop}>
+        <Typography variant="h6" className={classes.header}>
+          Filter Roomates:
         </Typography>
-        <Slider
-          value={filters.ageRange}
-          onChange={handleAgeRangeChange}
-          valueLabelDisplay="auto"
-          aria-labelledby="range-slider"
-          marks
-          min={16}
-          max={60}
-          step={1}
-        />
-      </div>
-      <div>
-        <FormControlLabel
-          control={(
-            <Checkbox
-              checked={filters.smoke}
-              onChange={handleChange}
-              name="smoke"
-              color="primary"
-            />
-          )}
-          label="No smoking"
-        />
-        <FormControlLabel
-          control={(
-            <Checkbox
-              checked={filters.pet}
-              onChange={handleChange}
-              name="pet"
-              color="primary"
-            />
-          )}
-          label="No pets"
-        />
-        <FormControlLabel
-          control={(
-            <Checkbox
-              checked={filters.relationship}
-              onChange={handleChange}
-              name="relationship"
-              color="primary"
-            />
-          )}
-          label="Single"
-        />
-        <FormControlLabel
-          control={(
-            <Checkbox
-              checked={filters.religion}
-              onChange={handleChange}
-              name="religion"
-              color="primary"
-            />
-          )}
-          label="Religion"
-        />
-        <FormControlLabel
-          control={(
-            <Checkbox
-              checked={filters.employed}
-              onChange={handleChange}
-              name="employed"
-              color="primary"
-            />
-          )}
-          label="Employed"
-        />
-      </div>
-      <div className={classes.range_root}>
-        <Typography id="range-slider" gutterBottom>
-          Budget range
-        </Typography>
-        <Slider
-          value={filters.budgetRange}
-          onChange={handleBudjetRangeChange}
-          valueLabelDisplay="auto"
-          aria-labelledby="range-slider"
-          marks
-          min={500}
-          max={6000}
-          step={500}
-        />
-      </div>
-      <div className={classes.buttons}>
-        <div>
-          <Button variant="contained" color="primary" onClick={handleSubmit} size="small">
-            Search
-          </Button>
+        <FormControl variant="filled" className={classes.formControl}>
+          <InputLabel id="demo-simple-select-filled-label">Gender</InputLabel>
+          <Select
+            labelId="demo-simple-select-filled-label"
+            id="demo-simple-select-filled"
+            defaultValue={filters.gender}
+            name="gender"
+            value={filters.gender}
+            onChange={(event) => handleGenderChange(event)}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value="male">Male</MenuItem>
+            <MenuItem value="female">Female</MenuItem>
+          </Select>
+        </FormControl>
+        <div className={classes.range_root}>
+          <Typography id="range-slider" gutterBottom>
+            Age range
+          </Typography>
+          <Slider
+            value={filters.ageRange}
+            onChange={handleAgeRangeChange}
+            valueLabelDisplay="auto"
+            aria-labelledby="range-slider"
+            marks
+            min={16}
+            max={60}
+            step={1}
+          />
         </div>
         <div>
-          <Button variant="contained" color="primary" onClick={handleRefreshSearch} size="small">
-            Clean Filters
-          </Button>
+          <FormControlLabel
+            control={(
+              <Checkbox
+                checked={filters.smoke}
+                onChange={handleChange}
+                name="smoke"
+                color="primary"
+              />
+            )}
+            label="No smoking"
+          />
+          <FormControlLabel
+            control={(
+              <Checkbox
+                checked={filters.pet}
+                onChange={handleChange}
+                name="pet"
+                color="primary"
+              />
+            )}
+            label="No pets"
+          />
+          <FormControlLabel
+            control={(
+              <Checkbox
+                checked={filters.relationship}
+                onChange={handleChange}
+                name="relationship"
+                color="primary"
+              />
+            )}
+            label="Single"
+          />
+          <FormControlLabel
+            control={(
+              <Checkbox
+                checked={filters.religion}
+                onChange={handleChange}
+                name="religion"
+                color="primary"
+              />
+            )}
+            label="Religion"
+          />
+          <FormControlLabel
+            control={(
+              <Checkbox
+                checked={filters.employed}
+                onChange={handleChange}
+                name="employed"
+                color="primary"
+              />
+            )}
+            label="Employed"
+          />
         </div>
-      </div>
-    </FormGroup>
+        <div className={classes.range_root}>
+          <Typography id="range-slider" gutterBottom>
+            Budget range
+          </Typography>
+          <Slider
+            value={filters.budgetRange}
+            onChange={handleBudjetRangeChange}
+            valueLabelDisplay="auto"
+            aria-labelledby="range-slider"
+            marks
+            min={500}
+            max={6000}
+            step={500}
+          />
+        </div>
+        <div className={classes.buttons}>
+          <div>
+            <Button variant="contained" color="primary" onClick={handleSubmit} size="small">
+              Search
+            </Button>
+          </div>
+          <div>
+            <Button variant="contained" color="primary" onClick={handleRefreshSearch} size="small">
+              Clean Filters
+            </Button>
+          </div>
+        </div>
+      </FormGroup>
+      <FormGroup className={classes.searchBarMobile}>
+        <Button variant="contained" color="primary" onClick={() => SetOpenFilter((prev) => !prev)} size="small">
+          Filter Roomates
+        </Button>
+        <FilterModal
+          openFilter={openFilter}
+          SetOpenFilter={SetOpenFilter}
+          setAllUsersInfo={setAllUsersInfo}
+          userId={userId}
+        />
+      </FormGroup>
+    </>
   );
 };
 
